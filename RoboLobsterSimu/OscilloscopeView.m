e46b8215d25f96321f8954118b88fc9d225464e7
qhdo1010 <do.q@husky.neu.edu>
2018-01-23 14:35:25 -0500

    Update OscilloscopeView.m
    invert trace order

--- RoboLobsterSimu/OscilloscopeView.m
+++ RoboLobsterSimu/OscilloscopeView.m
@@ -152,168 +152,6 @@ int minorLineWidth	 = 1;
     }
 }
 
-
-- (void)drawRect:(NSRect)dirtyRect
-{
-    [super drawRect:dirtyRect];
-    //[[self window] setFrameOrigin:NSMakePoint(0, dirtyRect.size.height)];
-    [[self window] setAllowsConcurrentViewDrawing:YES];
-    // [self setCanDrawConcurrently:YES];
-    [NSGraphicsContext saveGraphicsState];
-    if ([(AppDelegate *)[[NSApplication sharedApplication] delegate] switchColor] == 0){
-        [[NSColor blackColor] setFill];
-    }
-    else{
-        [[NSColor whiteColor] setFill];
-    }
-    [[NSBezierPath bezierPathWithRoundedRect:[self bounds] xRadius:0.0 yRadius:0.0] fill];
-    // [NSGraphicsContext restoreGraphicsState];
-    id propertyValue = [(AppDelegate *)[[NSApplication sharedApplication] delegate] traceWaveforms];
-    int numTr = [[propertyValue ipbuf] count];
-    
-    // [self drawXGrid:dirtyRect];
-    // [self drawYGrid:dirtyRect];
-    NSMutableArray*container; //contain stuff to be drawn
-    int width = dirtyRect.size.width;
-    int height = dirtyRect.size.height/numTr;
-    for (int j = 0 ; j < numTr; j ++){
-        container = [[NSMutableArray alloc] init];
-        // points = [[propertyValue ipbuf] objectAtIndex:j];
-        // [container addObject: points];
-        [container addObject: [NSNumber numberWithInteger:j]];
-        [container addObject: [NSNumber numberWithInteger:width]];
-        [container addObject: [NSNumber numberWithInteger:height]];
-        [self performSelector: @selector(drawTraces:) withObject:container];
-        [NSGraphicsContext saveGraphicsState];
-    }
-}
-
-
--(void) drawTraces :(NSMutableArray*) container{
-    [NSGraphicsContext restoreGraphicsState];
-    id propertyValue = [(AppDelegate *)[[NSApplication sharedApplication] delegate] traceWaveforms];
-    int idy = [[container objectAtIndex:0] intValue];
-    
-    NSMutableArray*points = [[propertyValue ipbuf] objectAtIndex:idy];
-    int width = [[container objectAtIndex:1] intValue];
-    int height =  [[container objectAtIndex:2] intValue];
-    int numberOfPoints = (int)[points count];
-    //NSLog(@"%@", points);
-    //int numberOfSineCurves = 30;
-    //float cWidth = 1;
-    float cWidth = ((float)width)/[(AppDelegate *)[[NSApplication sharedApplication] delegate] sweepDuration];
-   // NSLog(@"duration of %d = %f", idy ,[[[(AppDelegate *)[[NSApplication sharedApplication] delegate] durationArray] objectAtIndex:idy] floatValue]);
-   // float cWidth = 0.5;
-    float cHeight = height/10;
-    NSBezierPath *sinePath = [NSBezierPath bezierPath];
-    float gain = [[[(AppDelegate *)[[NSApplication sharedApplication] delegate] traceGainArray] objectAtIndex:idy] floatValue];
-    float offset = [[[(AppDelegate *)[[NSApplication sharedApplication] delegate] traceOffsetArray] objectAtIndex:idy] floatValue];
-    
-    [sinePath setLineWidth:1.0];
-    //        [sinePath setFlatness:3.0];
-    [sinePath moveToPoint:NSMakePoint(0*cWidth,  height/2 - [[points objectAtIndex:0] floatValue]*cHeight*gain + height*idy + offset)];
-    
-    
-    for (int i=0; i < numberOfPoints; i++)
-    {
-        if ([(AppDelegate *)[[NSApplication sharedApplication] delegate] switchColor] == 0){
-            if ([(AppDelegate *)[[NSApplication sharedApplication] delegate] IDofCellChosen]== idy){
-                [[NSColor redColor] set];
-            }
-            else{
-                [[NSColor whiteColor] set];
-            }
-            
-        }
-        else{
-            if ([(AppDelegate *)[[NSApplication sharedApplication] delegate] IDofCellChosen]== idy){
-                [[NSColor redColor] set];
-            }
-            else{
-            [[NSColor blackColor] set];
-            }
-        }
-       // [[NSColor whiteColor] set];
-
-        
-        if (i < (numberOfPoints - 1)){
-            [sinePath lineToPoint:NSMakePoint(i*cWidth,  height/2 - [[points objectAtIndex:i+1] floatValue]*cHeight*gain + height*idy + offset )];
-           // [sinePath setLineWidth:i];
-        }
-        //NSLog(@"%@",[points objectAtIndex:i+1]);
-        
-        
-    }
-    [sinePath stroke];
-}
-@end
-
-
-
-////////////////////////OLD CODE///////////////////////////////////////
-
-
-/*
- - (void)drawRect:(NSRect)rect   //mainly function calls to more complex implementation
- {
- // [super drawRect:rect];
- //  [NSGraphicsContext saveGraphicsState];
- 
- AppDelegate *appDelegate = [[NSApplication sharedApplication] delegate];
-	if (![[appDelegate analysisWindowController] scopeInited])
- [[[appDelegate analysisWindowController] oscilloscopeController] initScopeSweep];
-	//		return;
- 
- //   NSRect cRect = [[[appDelegate analysisWindowController] myTabView] contentRect];
- NSLog(@"Executing drawRect from  Oscilloscope View");
- // 	NSLog(@"TabView contentRect %g , %g, w %d h %d\n", NSMinX(cRect), NSMinY(cRect), (int) NSWidth(cRect), (int) NSHeight(cRect));
- //	NSLog(@"Oscilloscope rect on drawRect Entry %g , %g, w %d h %d\n", NSMinX(rect), NSMinY(rect), (int) NSWidth(rect), (int) NSHeight(rect));
- //    [[[appDelegate analysisWindowController] myCurrentView] lockFocus];
- //    self.drawXGridFlag = YES;
- //     [delegate setDrawYGridFlag:YES];
- //[[[appDelegate analysisWindowController] graphViewController] setGRect:rect];
- 
-	
-	if (![appDelegate drawChart]){	//Drawing The Oscilloscope Display
- NSLog(@"Executing Scope drawRect from  Oscilloscope View");
- [[NSColor blackColor] set];
- [NSBezierPath fillRect: rect];
- [self drawXGrid:rect];
- [self drawYGrid:rect];
- //     [NSGraphicsContext restoreGraphicsState];
- 
- //            if ([[[appDelegate analysisWindowController] oscilloscopeController] doInitAndScale] != NO) {
- NSLog(@"does init and Scale");
- //            if(![[appDelegate analysisWindowController] scopeInited])
- //                [[[appDelegate analysisWindowController] oscilloscopeController] initScopeSweep];
- [[[appDelegate analysisWindowController] oscilloscopeController] scaleScopeSweep];
- [self drawScopeSweep];   //This woeks when set to self
- // }
- if ([appDelegate drawChart]){	//Drawing the Chart Display
- NSLog(@"Executing Chart drawRect from  Oscilloscope View");
- 
- [[NSColor whiteColor] set];
- [NSBezierPath fillRect: rect];
- 
- [self drawXGrid:rect];
- [self drawYGrid:rect];
- 
- if ([[[appDelegate analysisWindowController] oscilloscopeController] doInitAndScale] != NO) {
- NSLog(@"does init and Scale");
- //                if(![[appDelegate analysisWindowController] scopeInited])
- //                   [[[appDelegate analysisWindowController] oscilloscopeController] initScopeSweep];
- [[[appDelegate analysisWindowController] oscilloscopeController] scaleScopeSweep];
- [self drawScopeSweep ];
- }
- [self drawScopeSweep];
- }
- 
- //    [[[appDelegate analysisWindowController] myCurrentView] unlockFocus];
- //    NSLog(@"Finished drawRect in oscilloscopeView");
- }
- }
- */
-
 /*
  - (void) drawScopeSweep             //: (NSRect)rect
  {
@@ -560,3 +398,144 @@ int minorLineWidth	 = 1;
  }
  }
  */
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    //[[self window] setFrameOrigin:NSMakePoint(0, dirtyRect.size.height)];
+    [[self window] setAllowsConcurrentViewDrawing:YES];
+    // [self setCanDrawConcurrently:YES];
+    [NSGraphicsContext saveGraphicsState];
+    if ([(AppDelegate *)[[NSApplication sharedApplication] delegate] switchColor] == 0){
+        [[NSColor blackColor] setFill];
+    }
+    else{
+        [[NSColor whiteColor] setFill];
+    }
+    [[NSBezierPath bezierPathWithRoundedRect:[self bounds] xRadius:0.0 yRadius:0.0] fill];
+    // [NSGraphicsContext restoreGraphicsState];
+    id propertyValue = [(AppDelegate *)[[NSApplication sharedApplication] delegate] traceWaveforms];
+    int numTr = [[propertyValue ipbuf] count];
+    
+    // [self drawXGrid:dirtyRect];
+    // [self drawYGrid:dirtyRect];
+    NSMutableArray*container; //contain stuff to be drawn
+    int width = dirtyRect.size.width;
+    int height = dirtyRect.size.height/numTr;
+    for (int j = 0 ; j < numTr; j ++){
+        container = [[NSMutableArray alloc] init];
+        // points = [[propertyValue ipbuf] objectAtIndex:j];
+        // [container addObject: points];
+        [container addObject: [NSNumber numberWithInteger:j]];
+        [container addObject: [NSNumber numberWithInteger:width]];
+        [container addObject: [NSNumber numberWithInteger:height]];
+        [self performSelector: @selector(drawTraces:) withObject:container];
+        [NSGraphicsContext saveGraphicsState];
+        
+    }
+    
+    
+}
+
+
+-(void) drawTraces :(NSMutableArray*) container{
+    [NSGraphicsContext restoreGraphicsState];
+    id propertyValue = [(AppDelegate *)[[NSApplication sharedApplication] delegate] traceWaveforms];
+    int idy = [[container objectAtIndex:0] intValue];
+    
+    NSMutableArray*points = [[propertyValue ipbuf] objectAtIndex:idy];
+    int width = [[container objectAtIndex:1] intValue];
+    int height =  [[container objectAtIndex:2] intValue];
+    int numberOfPoints = (int)[points count];
+   // NSLog(@"%d", idy);
+    //int numberOfSineCurves = 30;
+    //float cWidth = 1;
+    float cWidth = ((float)width)/[[[(AppDelegate *)[[NSApplication sharedApplication] delegate] durationArray] objectAtIndex:idy] floatValue];
+   // NSLog(@"duration of %d = %f", idy ,[[[(AppDelegate *)[[NSApplication sharedApplication] delegate] durationArray] objectAtIndex:idy] floatValue]);
+   // float cWidth = 0.5;
+    float cHeight = height/10;
+    NSBezierPath *sinePath = [NSBezierPath bezierPath];
+    float gain = [[[(AppDelegate *)[[NSApplication sharedApplication] delegate] traceGainArray] objectAtIndex:idy] floatValue];
+    float offset = [[[(AppDelegate *)[[NSApplication sharedApplication] delegate] traceOffsetArray] objectAtIndex:idy] floatValue];
+    for (int i=0; i < numberOfPoints; i++)
+    {
+        if ([(AppDelegate *)[[NSApplication sharedApplication] delegate] switchColor] == 0){
+            [[NSColor whiteColor] set];
+        }
+        else{
+            [[NSColor blackColor] set];
+        }
+       // [[NSColor whiteColor] set];
+        [sinePath setLineWidth:1.0];
+            //        [sinePath setFlatness:3.0];
+        [sinePath moveToPoint:NSMakePoint(i*cWidth,  height/2 - [[points objectAtIndex:i] floatValue]*cHeight*gain + height*idy + offset)];
+        
+        if (i < (numberOfPoints - 1))
+            [sinePath lineToPoint:NSMakePoint(i*cWidth,  height/2 - [[points objectAtIndex:i+1] floatValue]*cHeight*gain + height*idy + offset)];
+        
+        
+    }
+    [sinePath stroke];
+}
+
+/*
+ - (void)drawRect:(NSRect)rect   //mainly function calls to more complex implementation
+ {
+ // [super drawRect:rect];
+ //  [NSGraphicsContext saveGraphicsState];
+ 
+ AppDelegate *appDelegate = [[NSApplication sharedApplication] delegate];
+	if (![[appDelegate analysisWindowController] scopeInited])
+ [[[appDelegate analysisWindowController] oscilloscopeController] initScopeSweep];
+	//		return;
+ 
+ //   NSRect cRect = [[[appDelegate analysisWindowController] myTabView] contentRect];
+ NSLog(@"Executing drawRect from  Oscilloscope View");
+ // 	NSLog(@"TabView contentRect %g , %g, w %d h %d\n", NSMinX(cRect), NSMinY(cRect), (int) NSWidth(cRect), (int) NSHeight(cRect));
+ //	NSLog(@"Oscilloscope rect on drawRect Entry %g , %g, w %d h %d\n", NSMinX(rect), NSMinY(rect), (int) NSWidth(rect), (int) NSHeight(rect));
+ //    [[[appDelegate analysisWindowController] myCurrentView] lockFocus];
+ //    self.drawXGridFlag = YES;
+ //     [delegate setDrawYGridFlag:YES];
+ //[[[appDelegate analysisWindowController] graphViewController] setGRect:rect];
+ 
+	
+	if (![appDelegate drawChart]){	//Drawing The Oscilloscope Display
+ NSLog(@"Executing Scope drawRect from  Oscilloscope View");
+ [[NSColor blackColor] set];
+ [NSBezierPath fillRect: rect];
+ [self drawXGrid:rect];
+ [self drawYGrid:rect];
+ //     [NSGraphicsContext restoreGraphicsState];
+ 
+ //            if ([[[appDelegate analysisWindowController] oscilloscopeController] doInitAndScale] != NO) {
+ NSLog(@"does init and Scale");
+ //            if(![[appDelegate analysisWindowController] scopeInited])
+ //                [[[appDelegate analysisWindowController] oscilloscopeController] initScopeSweep];
+ [[[appDelegate analysisWindowController] oscilloscopeController] scaleScopeSweep];
+ [self drawScopeSweep];   //This woeks when set to self
+ // }
+ if ([appDelegate drawChart]){	//Drawing the Chart Display
+ NSLog(@"Executing Chart drawRect from  Oscilloscope View");
+ 
+ [[NSColor whiteColor] set];
+ [NSBezierPath fillRect: rect];
+ 
+ [self drawXGrid:rect];
+ [self drawYGrid:rect];
+ 
+ if ([[[appDelegate analysisWindowController] oscilloscopeController] doInitAndScale] != NO) {
+ NSLog(@"does init and Scale");
+ //                if(![[appDelegate analysisWindowController] scopeInited])
+ //                   [[[appDelegate analysisWindowController] oscilloscopeController] initScopeSweep];
+ [[[appDelegate analysisWindowController] oscilloscopeController] scaleScopeSweep];
+ [self drawScopeSweep ];
+ }
+ [self drawScopeSweep];
+ }
+ 
+ //    [[[appDelegate analysisWindowController] myCurrentView] unlockFocus];
+ //    NSLog(@"Finished drawRect in oscilloscopeView");
+ }
+ }
+ */
+@end
