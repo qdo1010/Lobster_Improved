b8963d4d4dc87e62789af07194f082f18f80e2b9
qhdo1010 <do.q@husky.neu.edu>
2018-02-06 14:48:21 -0500

    hi

--- RoboLobsterSimu/pc2dspLobster.c
+++ RoboLobsterSimu/pc2dspLobster.c
@@ -63,7 +63,7 @@ void spikingNeuronInit(struct structSpiking *ptr) {
     ptr->spike = 0;
     ptr->alpha = 4;
    // ptr->alpha = 4.0;//3.85;     //sets the type of neuron spiking (alpha < 4) or bursting (alphs > 4)
-    ptr->sigma = .1;      //sets the baseline state of the neuron (quiet or spiking/ bursting)
+    ptr->sigma = .2;      //sets the baseline state of the neuron (quiet or spiking/ bursting)
 //   ptr->sigma = 0.46;      //sets the baseline state of the neuron (quiet or spiking/ bursting)
     ptr->sigmaE = 1.0;     //sets the sensitivity to excitatory synaptic current
     ptr->sigmaI = 1.0;     //sets the sensitivity to inhibitory synaptic current
