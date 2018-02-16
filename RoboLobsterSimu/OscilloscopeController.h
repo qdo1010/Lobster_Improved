ac2534a4743dd601563d0b26c784450cb76f9b37
qhdo1010 <do.q@husky.neu.edu>
2018-02-16 14:36:15 -0500

    add load and save func

--- RoboLobsterSimu/OscilloscopeController.h
+++ RoboLobsterSimu/OscilloscopeController.h
@@ -90,7 +90,7 @@ extern long elapsed;
 - (IBAction)changeView:(id)sender;
 - (IBAction)setParams:(id)sender;
 - (IBAction)loadParams:(id)sender;
-
+- (IBAction)saveParams:(id)sender;
 
 - (void)beginSendingStuffToBeDraw;
 
